e2d406eaee99c4520c29bae16f388e0ccc1f48ec
Daniel <daniel.elport@itc-vogt.de>
2020-05-12 18:10:39 +0200

    cors pass

--- express/server.js
+++ express/server.js
@@ -1,15 +1,10 @@
 'use strict';
 const express = require('express');
-const path = require('path');
 const serverless = require('serverless-http');
 const app = express();
-
+const request = require('request');
 const router = express.Router();
-router.get('/*', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
-  res.end();
-});
+
 
 app.use((req, res, next) => {
 
@@ -17,10 +12,40 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept');
+
   next();
 });
 app.use('/.netlify/functions/server', router);  // path must route to lambda
-app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+app.use('/', (req, res) => {
+
+  const url = req.url.substring(1);
+  // get data from request url
+  request( url,{
+    uri: url,
+    method: 'GET',
+    encoding: null
+  }, (error, response, body) => {
+
+    if (!error) {
+
+      // copy headers
+      Object.keys(response.headers).forEach(key => {
+        res.setHeader(key, response.headers[key]);
+      });
+
+      // forward http response
+      res.status(200);
+      res.send(body);
+
+    } else {
+
+      // handle error
+      res.status(400);
+      res.json({error: error.message});
+    }
+  });
+
+});
 
 module.exports = app;
 module.exports.handler = serverless(app);
